2bdc503d778365283683d1394b62da9716bb563e
calicanadian <ryan.stuhl@gmail.com>
2019-08-20 12:11:29 -0700

    Adding StyleRoot component from Radium to allow for dynamic media queries

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Radium from 'radium';
+import Radium, { StyleRoot } from 'radium';
 import './App.css';
 import Person from './Person/Person';
 
@@ -92,12 +92,14 @@ class App extends Component {
     }
 
     return (
-      <div className="App">
-        <h1>Hi, I'm a react app</h1>
-        <p className={classes.join(' ')}>This is really working!!</p>
-        <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
-        {persons}
-      </div>
+      <StyleRoot>
+        <div className="App">
+          <h1>Hi, I'm a react app</h1>
+          <p className={classes.join(' ')}>This is really working!!</p>
+          <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+          {persons}
+        </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, "Does this work now?"));
   }
