2f798394e828d03d1bc5e70c74ae0a070dbc8ac9
calicanadian <ryan.stuhl@gmail.com>
2019-08-20 12:24:44 -0700

    Removing the Radium library and usage

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
-import Radium, { StyleRoot } from 'radium';
-import './App.css';
+import styles from './App.css';
 import Person from './Person/Person';
 
 class App extends Component {
@@ -55,11 +54,7 @@ class App extends Component {
       border: '1px solid blue',
       padding: '8px',
       fontSize: '14px',
-      cursor: 'pointer',
-      ':hover': {
-        backgroundColor: 'lightgreen',
-        color: 'black'
-      }
+      cursor: 'pointer'
     };
 
     let persons = null;
@@ -80,7 +75,6 @@ class App extends Component {
         </div>
       );
       style.backgroundColor = 'red';
-      style[':hover'] = { backgroundColor: 'salmon', color: 'black' }
     }
 
     const classes = [];
@@ -92,17 +86,15 @@ class App extends Component {
     }
 
     return (
-      <StyleRoot>
-        <div className="App">
-          <h1>Hi, I'm a react app</h1>
-          <p className={classes.join(' ')}>This is really working!!</p>
-          <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
-          {persons}
-        </div>
-      </StyleRoot>
+      <div className={styles.App}>
+        <h1>Hi, I'm a react app</h1>
+        <p className={classes.join(' ')}>This is really working!!</p>
+        <button style={style} onClick={this.togglePersonsHandler}>Switch Name</button>
+        {persons}
+      </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1',null, "Does this work now?"));
   }
 }
 
-export default Radium(App);
+export default App;
